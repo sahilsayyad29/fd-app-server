22e4591c040b70f1b66639db788ac3968aa2c97b
Sahil Sayyad <sahilsayyadofficial@gmail.com>
2026-09-06 23:07:49 +0530

    Initial commit

---
+++ src/test/java/com/fd/server/FdAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fd.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
